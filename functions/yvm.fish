df6af7d6a713668a5021cbd37cf1b52b785ad129
Florian B <yuuki@protonmail.com>
2019-10-19 16:04:42 +0200

    Multiple improvements
    Improved handling of "$_flag_f" force fetch. Note the errors I had been
    seeing earlier when trying to pass the flag and re-parse it were totally
    unrelated to the flag and parsing. I was using echo to debug and that
    echo would of course later  on be redirected into a function ~_~ where
    it caused an error.
    
    Improved error messages. Now checks if there's a yarn.js in /bin. Also
    prints more info on failing curl.
    
    Improved help command with info about supported versions.
    
    It now uses the tarball URL from the release to fetch the tarball which
    should be more resilient.

--- functions/yvm.fish
+++ functions/yvm.fish
@@ -1,17 +1,17 @@
-set -g yvm_fish 0.5.0
+set -g yvm_fish 0.6.0
 
 function yvm -a cmd -d "yarn version manager"
-    set -l options (fish_opt -s h -l help) (fish_opt -s v -l version)
+    set -l options (fish_opt -s h -l help) (fish_opt -s v -l version) (fish_opt -s f -l force-fetch)
     argparse $options -- $argv
 
     if test -n "$_flag_h"
-      _yvm_help
-      return 0
+        _yvm_help
+        return 0
     end
 
     if test -n "$_flag_v"
-      echo "$yvm_fish"
-      return 0
+        echo "$yvm_fish"
+        return 0
     end
 
     set -q XDG_CONFIG_HOME
@@ -27,23 +27,26 @@ function yvm -a cmd -d "yarn version manager"
     switch "$cmd"
         case ls list
             set -e argv[1]
-            _yvm_ls $argv
+            _yvm_ls "$_flag_f"
         case use
             set -e argv[1]
-            _yvm_use $argv
+            _yvm_use "$_flag_f"
         case rm
             set -e argv[1]
-            _yvm_rm $argv
-        case ""
-            echo HELLo
+            _yvm_rm "$_flag_f"
+        case "help"
+            _yvm_help
         case \*
-            echo "yvm: unknown flag or command \"$cmd\"" >&2
-            _yvm_help >&2
+            echo "yvm: unknown flag or command \"$cmd\""
+            _yvm_help
             return 1
     end
 end
 
 function _yvm_get_releases
+    set -l options (fish_opt -s f -l force-fetch)
+    argparse $options -- $argv
+
     set -q yvm_last_updated
     or set -g yvm_last_updated 0
     set -l releases "$yvm_config/yarn_releases"
@@ -79,15 +82,8 @@ function _yvm_use
     set -l options (fish_opt -s f -l force-fetch)
     argparse $options -- $argv
 
-    set -l force_fetch 0
+    set -l releases (_yvm_get_releases "$_flag_f")
 
-    if test -n "$_flag_f"
-        set force_fetch 1
-    end
-
-    set -l releases (_yvm_get_releases "$force_fetch")
-
-    set -l yarn_version
     set -l version_to_install $argv
 
     if test $version_to_install = "latest"
@@ -100,40 +96,50 @@ function _yvm_use
     end
 
     if not test -d "$yvm_config/$version_to_install"
-        set -l tarball_base_name "yarn-v$version_to_install"
+        set -l url (cat $releases | grep $version_to_install | awk '{ print $2 }')
 
-        set -l url "https://yarnpkg.com/downloads/$version_to_install/$tarball_base_name.tar.gz"
-
-        echo "fetching $url" >&2
+        echo "fetching $url..." >&2
 
         set -l temp_dir (command mktemp -d -t "yvm-yarn-$version_to_install-XXXXXXXXXXXXX")
+        set -l temp_file (command mktemp "yvm-yarn-$version_to_install-tarball-XXXXXXXXXX")
 
-        if not command curl -L --fail --progress-bar $url | command tar -xzf- -C $temp_dir
+        if not command curl -L --fail --progress-bar $url -o $temp_file 2>/dev/null
             command rm -rf $temp_dir
-            echo "yvm: fetch error -- are you offline?" >&2
+            command rm $temp_file
+
+            echo "Couldn't download the tarball from url:"
+            echo "$url"
+            echo "Are you offline?"
             return 1
         end
 
         command mkdir -p "$yvm_config/$version_to_install/"
 
-        # Newer yarn versions have /yarn_version/bin in their tarball
-        if test -d "$temp_dir/$tarball_base_name/"
-            command mv $temp_dir/$tarball_base_name/* "$yvm_config/$version_to_install/"
-            # Older yarn versions have just /dist/bin and /dist/lib in the tarball
-        else if test -d "$temp_dir/dist/"
-            command mv $temp_dir/dist/* "$yvm_config/$version_to_install/"
-        end
+        command tar -xzf $temp_file -C $temp_dir
+
+        set -l yarn_pkg_path (find $temp_dir -maxdepth 1 -mindepth 1 -type d)
+        command mv $yarn_pkg_path/* "$yvm_config/$version_to_install/"
 
         command rm -r $temp_dir
+        command rm $temp_file
     end
 
     if not test -d "$yvm_config/$version_to_install/"
-      echo "Failed to install yarn version \"$version_to_install\", but curl didn't error. Please report this bug."
-      return 1
+        echo "Failed to install yarn version \"$version_to_install\", but curl didn't error. Please report this bug."
+        return 1
+    end
+
+    if not test -e "$yvm_config/$version_to_install/bin/yarn.js"
+        echo "Yarn was installed but there is no yarn.js in \"$yvm_config/$version_to_install/bin/yarn.js\"."
+        echo "This yarn version is either really old, and exports for example a kpm.js, or it's a version that needs be built from source."
+        echo ""
+        echo "Note that the yarn version is not removed from \"$yvm_config/\", but it's also not prepended to \$fish_user_paths. It's advised to use \"yvm rm\" to remove the version again."
+        return 1
     end
 
     if test -s "$yvm_config/version"
         read -l last <"$yvm_config/version"
+
         if set -l i (contains -i -- "$yvm_config/$last/bin" $fish_user_paths)
             set -e fish_user_paths[$i]
         end
@@ -150,13 +156,7 @@ function _yvm_rm
     set -l options (fish_opt -s f -l force-fetch)
     argparse $options -- $argv
 
-    set -l force_fetch 0
-
-    if test -n "$_flag_f"
-        set force_fetch 1
-    end
-
-    set -l releases (_yvm_get_releases "$force_fetch")
+    set -l releases (_yvm_get_releases "$_flag_f")
 
     set -l yarn_version $argv[1]
     read -l active_version <"$yvm_config/version"
@@ -185,13 +185,8 @@ end
 function _yvm_ls
     set -l options (fish_opt -s f -l force-fetch)
     argparse $options -- $argv
-    set -l force_fetch 0
 
-    if test -n "$_flag_f"
-        set force_fetch 1
-    end
-
-    set -l releases (_yvm_get_releases "$force_fetch")
+    set -l releases (_yvm_get_releases "$_flag_f")
     set -l yarn_version
 
     if test -f "$yvm_config/version"
@@ -224,17 +219,23 @@ function _yvm_ls
 end
 
 function _yvm_help
-    echo "usage: yvm --help           Show this help"
+    echo "usage: yvm help/--help/-h   Show this help"
     echo "       yvm --version        Show the current version of yvm"
     echo "       yvm use <version>    Download <version> and modify PATH to use it."
     echo "                            Needs to be the exact version from ls."
     echo "       yvm ls/list          List all versions including if they're installed and/or active"
     echo "       yvm rm               Remove specified version from file system and PATH."
     echo "                            Needs to be the exact version from ls."
-    echo "       yvm -f/--force-fetch Force fetch the releases from remote before use or ls"
+    echo "       -f/--force-fetch     Force fetch the releases from remote before \"use\" or \"ls\""
+    echo "                            Release data is cached for 120 seconds"
+    echo ""
     echo "examples:"
     echo "       yvm use 1.17.3"
     echo "       yvm use latest"
     echo "       yvm ls"
     echo "       yvm ls -f"
+    echo ""
+    echo "Important:"
+    echo "       Does not support old versions exporting kpm.js, nor versions that need to be"
+    echo "       built from source."
 end
